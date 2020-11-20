21e4cb439197251660b358b21dc25681779afd80
VahidRasekhi <v.rasekhii@gmail.com>
2020-11-20 12:21:10 +0330

    first Commit for reactjs project

---
+++ src/components/NavItem.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Route , Link } from 'react-router-dom';
+
+class NavItem extends Component {
+    render() {
+        console.log(this.props);
+        const { to , activeOnlyWhenExact , children } = this.props;
+        return (
+            <Route path={to} exact={activeOnlyWhenExact} children={({ match }) => (
+                <li className={['nav-item' , match ? 'active' : ''].join(' ')}>
+                    <Link className="nav-link" to={to}>{children}</Link>
+                </li>
+            )}/>
+        );
+    }
+}
+
+export default NavItem;
